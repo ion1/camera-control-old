9e6690cd5fa733330acdac4241f9d573bc1384d6
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-23 19:44:22 +0300

    Add control-board video-selector

---
+++ new/control-board/video-selector.rb
@@ -0,0 +1,20 @@
+require 'cc/parport'
+require 'cc/singleton'
+
+module ControlBoard
+  class VideoSelector
+    def initialize
+      @port = CC::ParPort.singleton
+    end
+
+    def select num
+      # Currently only two video inputs are supported.
+      control = @port.control
+
+      control &= (0x01 ^ 0xff)
+      control |= 0x01 if num != 0
+
+      @port.control = control
+    end
+  end
+end
