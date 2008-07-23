e0193bb3640976ac676ab4168e54c7b947d3924b
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-23 22:42:06 +0300

    ssp_camera_slot: Rename taken as allocated

--- new/db/models/ssp_camera_slot.rb
+++ new/db/models/ssp_camera_slot.rb
@@ -2,8 +2,8 @@ class SspCameraSlot < ActiveRecord::Base
   belongs_to :ssp_camera
   belongs_to :target
 
-  named_scope :free,  :conditions => ['target_id IS NULL']
-  named_scope :taken, :conditions => ['target_id IS NOT NULL']
+  named_scope :free,      :conditions => ['target_id IS NULL']
+  named_scope :allocated, :conditions => ['target_id IS NOT NULL']
 
   validates_presence_of  :slot
   validates_inclusion_of :slot, :in => (1..127)
