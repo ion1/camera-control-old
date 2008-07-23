9c24e84468b91e30988eb9bae2d89a7a3dd71652
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-23 19:30:04 +0300

    Add new callbacks

---
+++ new/cc/callbacks.rb
@@ -0,0 +1,16 @@
+module CC
+  class Callbacks
+    def initialize
+      @callbacks = []
+    end
+
+    def add &block
+      @callbacks << block
+    end
+
+    def call *args
+      @callbacks.each {|cb| cb.call *args }
+    end
+  end
+end
+
