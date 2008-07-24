9469b01bf125a7fe7359b051b291e481da48912f
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-24 14:39:25 +0300

    Rotate log files by default

--- cc/logger.rb
+++ cc/logger.rb
@@ -5,9 +5,8 @@ module CC
   class Logger < ::Logger
     LOG_ROOT = File.expand_path '~/.local/share/camera-control'
 
-    def initialize *args
-      args = [LOG_ROOT+'/camera-control.log'] if args.empty?
-      super(*args)
+    def initialize target=LOG_ROOT+'/camera-control.log'
+      super target, 10, 2**20
 
       self.progname = 'camera-control'
       self.level    = INFO
