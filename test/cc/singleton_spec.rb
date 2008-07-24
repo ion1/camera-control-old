6f073dcd20a355b962c029d20ca8921f9a7c0070
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-25 01:12:23 +0300

    Line up similar code

--- test/cc/singleton_spec.rb
+++ test/cc/singleton_spec.rb
@@ -8,10 +8,10 @@ describe CC::Singleton do
     Array.singleton          << :foo
     Array.singleton([0,1,2]) << :bar
 
-    Array.singleton.should == [:foo]
+    Array.singleton         .should == [:foo]
     Array.singleton([0,1,2]).should == [0,1,2,:bar]
 
-    Array.singleton.object_id.should == a0.object_id
+    Array.singleton.object_id         .should == a0.object_id
     Array.singleton([0,1,2]).object_id.should == a1.object_id
   end
 end
