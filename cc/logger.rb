44f5a8a255922886c8ded5b9e9f0862c5fe8d11b
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-25 12:56:50 +0300

    logger: Make the log directory automatically

--- cc/logger.rb
+++ cc/logger.rb
@@ -6,6 +6,10 @@ module CC
     LOG_ROOT = File.expand_path '~/.local/share/camera-control'
 
     def initialize target=LOG_ROOT+'/camera-control.log'
+      if target.is_a? String
+        FileUtils.mkdir_p File.dirname(target)
+      end
+
       super target, 10, 2**20
 
       self.progname = 'camera-control'
