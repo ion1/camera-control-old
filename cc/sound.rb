e81f1f8b8b8f475b63eb74bf0eb069678500f4bb
Johan Kiviniemi <devel@johan.kiviniemi.name>
2008-07-26 18:33:01 +0300

    Add initial support for speech using festival

--- cc/sound.rb
+++ cc/sound.rb
@@ -1,8 +1,25 @@
+require 'iconv'
+
 module CC
   module Sound
     def self.info
       Thread.new do
         system "/usr/bin/aplay", "/usr/share/sounds/info.wav"
+      end
+    end
+
+    def self.speak language, text
+      Thread.new do
+        IO.popen '-', 'w' do |io|
+          unless io
+            # Child.
+            exec '/usr/bin/festival', '-b', '--language', language,
+                 '(tts_file "-")'
+            exit 1
+          end
+
+          io.puts Iconv.conv('latin1//translit', 'utf-8', text)
+        end
       end
     end
   end
